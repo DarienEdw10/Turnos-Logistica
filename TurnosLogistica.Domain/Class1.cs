72f76630fc8566f44e961e356f752ed1a7d5c477
EdwinGJ20 <tu-darienedwinj.@gmail.com>
2026-08-24 13:12:14 -0600

    Initial commit: Solución TurnosLogistica en .NET 8

---
+++ TurnosLogistica.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TurnosLogistica.Domain;
+
+public class Class1
+{
+
+}
---
+++ TurnosLogistica.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TurnosLogistica.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
